1e81cb6604f58f1ff81deb106ba64f57c093bcf8
jeolmi <jiyoon2014@daum.net>
2025-01-29 00:54:39 +0900

    1주차

---
+++ 01_web/03_js/js/out2.js
@@ -0,0 +1,15 @@
+function add() {
+  alert("더하기 기능 처리후 결과창");
+}
+function go() {
+  alert("go함수를 누르셨군요.!");
+}
+function move() {
+  alert("move함수를 누르셨군요.!");
+}
+function travel() {
+  alert("travel함수를 누르셨군요.!");
+}
+function rest() {
+  alert("rest함수를 누르셨군요.!");
+}
